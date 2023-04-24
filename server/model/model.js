dc9db7e001d70d515affede8f7a6b72b05aedcba
wilsonRodriguezz <wrodri10@my.centennialcollege.ca>
2023-04-24 18:54:13 -0400

    changes to the db schema

--- server/model/model.js
+++ server/model/model.js
@@ -1,29 +1,27 @@
 const mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true
-    },
-    message: {
-        type: String
-    },
-
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+  },
+});
 
 const Userdb = mongoose.model('userdb', schema);
 
-module.exports = Userdb; 
+module.exports = Userdb;

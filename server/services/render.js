a31ae0173c65053c0bd2735bbbb6a607ceae89e2
wilsonRodriguezz <wrodri10@my.centennialcollege.ca>
2023-04-24 18:28:41 -0400

    changes in axios request

--- server/services/render.js
+++ server/services/render.js
@@ -1,49 +1,50 @@
 const axios = require('axios');
 
-
-
 exports.homePage = (req, res) => {
-    res.render('index');
-}
+  res.render('index');
+};
 
 exports.contact = (req, res) => {
-    res.render('contact');
-}
+  res.render('contact');
+};
 
 exports.about = (req, res) => {
-    res.render('about');
-}
+  res.render('about');
+};
 
 exports.services = (req, res) => {
-    res.render('services');
-}
+  res.render('services');
+};
 
 exports.projects = (req, res) => {
-    res.render('projects');
-}
+  res.render('projects');
+};
 
 exports.secure = (req, res) => {
-    res.render('authentication')
-}
+  res.render('authentication');
+};
 
 exports.usersList = (req, res) => {
-    //make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
-        .then(function (response) {
-            res.render('index-contactList', { users: response.data });
-        })
-        .catch(err => {
-            res.send(err);
-        })
-}
+  //make a get request to /api/users
+  axios
+    .get('https://wilsonrodriguezzportfolio.onrender.com//api/users')
+    .then(function (response) {
+      res.render('index-contactList', { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 
 exports.updateUser = (req, res) => {
-    axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
-        .then(function (userdata) {
-            res.render('updateUser-contactList', { user: userdata.data })
-        })
-        .catch(err => {
-            res.send(err);
-        })
-}
-
+  axios
+    .get('https://wilsonrodriguezzportfolio.onrender.com//api/users', {
+      params: { id: req.query.id },
+    })
+    .then(function (userdata) {
+      res.render('updateUser-contactList', { user: userdata.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
